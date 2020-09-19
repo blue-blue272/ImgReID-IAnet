82775fbcce5d8d89cdb52105f828fb461ca9d8b0
blue-blue272 <56821684+blue-blue272@users.noreply.github.com>
2020-09-19 19:37:02 +0800

    Add files via upload

---
+++ data_manager/market1501_seg.py
@@ -0,0 +1,132 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import glob
+import re
+import sys
+import urllib
+import tarfile
+import zipfile
+import os.path as osp
+from scipy.io import loadmat
+import numpy as np
+import h5py
+from scipy.misc import imsave
+
+from .base import BaseImgDataset
+
+
+class Market1501_seg(BaseImgDataset):
+    """
+    Market1501
+
+    Reference:
+    Zheng et al. Scalable Person Re-identification: A Benchmark. ICCV 2015.
+
+    URL: http://www.liangzheng.org/Project/project_reid.html
+    
+    Dataset statistics:
+    # identities: 1501 (+1 for background)
+    # images: 12936 (train) + 3368 (query) + 15913 (gallery)
+    """
+    dataset_dir = '/home/houruibing/data/reid/image/Market-1501/'
+
+    def __init__(self, verbose=True, use_lmdb=False, **kwargs):
+        super(Market1501_seg, self).__init__()
+        self.train_dir = osp.join(self.dataset_dir, 'bounding_box_train')
+        self.query_dir = osp.join(self.dataset_dir, 'query')
+        self.gallery_dir = osp.join(self.dataset_dir, 'bounding_box_test')
+
+        self.train_seg_dir = osp.join(self.dataset_dir, 'seg_part4', 'Market1501_train_seg_part4')
+
+        self._check_before_run()
+
+        train, num_train_pids, num_train_imgs = self._process_dir(self.train_dir, self.train_seg_dir, relabel=True)
+        query, num_query_pids, num_query_imgs = self._process_dir(self.query_dir, relabel=False)
+        gallery, num_gallery_pids, num_gallery_imgs = self._process_dir(self.gallery_dir, relabel=False)
+        num_total_pids = num_train_pids + num_query_pids
+        num_total_imgs = num_train_imgs + num_query_imgs + num_gallery_imgs
+
+        if verbose:
+            print("=> Market1501 loaded")
+            print("Dataset statistics:")
+            print("  ------------------------------")
+            print("  subset   | # ids | # images")
+            print("  ------------------------------")
+            print("  train    | {:5d} | {:8d}".format(num_train_pids, num_train_imgs))
+            print("  query    | {:5d} | {:8d}".format(num_query_pids, num_query_imgs))
+            print("  gallery  | {:5d} | {:8d}".format(num_gallery_pids, num_gallery_imgs))
+            print("  ------------------------------")
+            print("  total    | {:5d} | {:8d}".format(num_total_pids, num_total_imgs))
+            print("  ------------------------------")
+
+        self.train = train
+        self.query = query
+        self.gallery = gallery
+
+        self.num_train_pids = num_train_pids
+        self.num_query_pids = num_query_pids
+        self.num_gallery_pids = num_gallery_pids
+
+        if use_lmdb:
+            self.generate_lmdb()
+
+    def _check_before_run(self):
+        """Check if all files are available before going deeper"""
+        if not osp.exists(self.dataset_dir):
+            raise RuntimeError("'{}' is not available".format(self.dataset_dir))
+        if not osp.exists(self.train_dir):
+            raise RuntimeError("'{}' is not available".format(self.train_dir))
+        if not osp.exists(self.query_dir):
+            raise RuntimeError("'{}' is not available".format(self.query_dir))
+        if not osp.exists(self.gallery_dir):
+            raise RuntimeError("'{}' is not available".format(self.gallery_dir))
+        if not osp.exists(self.train_seg_dir):
+            raise RuntimeError("'{}' is not available".format(self.train_dir))
+
+    def _process_dir(self, dir_path, seg_dir_path=None, relabel=False):
+        img_paths = glob.glob(osp.join(dir_path, '*.jpg'))
+        pattern = re.compile(r'([-\d]+)_c(\d)')
+
+        pid_container = set()
+        for img_path in img_paths:
+            pid, _ = map(int, pattern.search(img_path).groups())
+            if pid == -1: continue  # junk images are just ignored
+            pid_container.add(pid)
+        pid2label = {pid:label for label, pid in enumerate(pid_container)}
+
+        dataset = []
+        for img_path in img_paths:
+            pid, camid = map(int, pattern.search(img_path).groups())
+            if pid == -1: continue  # junk images are just ignored
+            assert 0 <= pid <= 1501  # pid == 0 means background
+            assert 1 <= camid <= 6
+            camid -= 1 # index starts from 0
+            if relabel: pid = pid2label[pid]
+
+            if seg_dir_path is not None:
+                img_name = img_path.split('/')[-1].split('.')[0]
+                seg_dir = osp.join(seg_dir_path, img_name)
+                head_path = osp.join(seg_dir, 'head.png')
+                upper_body_path = osp.join(seg_dir, 'upper_clothes.png')
+                lower_body_path = osp.join(seg_dir, 'lower_clothes.png')
+                shoes_path = osp.join(seg_dir, 'shoes.png')
+                foreground_path = osp.join(seg_dir, 'foreground.png')
+
+                assert os.path.exists(head_path)
+                assert os.path.exists(upper_body_path)
+                assert os.path.exists(lower_body_path)
+                assert os.path.exists(shoes_path)
+                assert os.path.exists(foreground_path)
+                img_path = [img_path, head_path, upper_body_path, lower_body_path, shoes_path, foreground_path]
+
+            dataset.append((img_path, pid, camid))
+
+        num_pids = len(pid_container)
+        num_imgs = len(dataset)
+        return dataset, num_pids, num_imgs
+    
+if __name__ == '__main__':
+    Market1501_seg()
